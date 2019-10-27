5bd130e98ea10e2690f1a166faf2992ebabaaaae
SravantiNomula <47375870+SravantiNomula@users.noreply.github.com>
2019-10-27 12:41:17 +0530

    Update Q3_MT2018524.s

--- ASSIGNMENT-1/Q3_MT2018524.s
+++ ASSIGNMENT-1/Q3_MT2018524.s
@@ -2,12 +2,12 @@ AREA     first, CODE, READONLY
      export __main	 
 	 ENTRY 
 __main function
-		  MOV R0,#0x53	; 
-		  AND R1,R0,#0x01 ; To have only the 1st bit from lsb side of the required number  
-		  CMP R1,#0x0 	; checking if this is 0 or 1(1=odd; 0=even)
+		  MOV r0,#0x53	; 
+		  AND r1,r0,#0x01 ; To have only the 1st bit from lsb side of the required number  
+		  CMP r1,#0x0 	; checking if this is 0 or 1(1=odd; 0=even)
 		  ITE EQ 					
-		  MOVEQ R2,#0x0	; if R1 = 0, move 0 to R2 which shows that the number is EVEN
-		  MOVNE R2,#0x1	; if R1 = 1, move 1 to R2 which shows that the number is ODD
+		  MOVEQ r2,#0x0	; if R1 = 0, reflect R2 as 0 which means the number is EVEN
+		  MOVNE r2,#0x1	; if R1 = 1, reflect R2 as 1 which means  the number is ODD
 STOP		      B STOP       ; stop program 
         endfunc
 end 
