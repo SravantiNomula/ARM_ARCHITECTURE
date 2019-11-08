6368f992e6c534fc50f6f840dd8bbce429e06f7e
SravantiNomula <47375870+SravantiNomula@users.noreply.github.com>
2019-11-08 13:01:12 +0530

    Update Q2_MT2018524.s

--- ASSIGNMENT-1/Q2_MT2018524.s
+++ ASSIGNMENT-1/Q2_MT2018524.s
@@ -9,23 +9,21 @@
 	 AREA     first, CODE, READONLY
      	 EXPORT __main
      	 ENTRY 
-__main  FUNCTION		 		
+__main  FUNCTION
+		;Loading the 3 numbers
 		MOV  r1, #5 
 		MOV  r2, #4 
 		MOV  r3, #8 
 		
-		
+		;comparing the 1st 2 numbers
 		CMP r1, r2	
-		BGE hey	
-		MOV r1,r2
-hey		CMP r1,r3
+		BGE hey		;if r1>r2 or r1=r2 branch to hey  
+		MOV r1,r2	; if r1 was < r2 then copy r2 into r1 as r1 is supposed to have largest number finally
+		
+hey		CMP r1,r3	; if r1>r3 or r1=r3  then stop
 		BGE stop
 		MOV r1,r3
 		
-	   ;LDR r2,=0x20000040
-	   ;LDRB r0,[r2],#0x5
-	   ;LSL r1,r0,#1
-	   ;LSL r2,r2,#1
 	   
 stop    B stop ; stop program
      ENDFUNC
