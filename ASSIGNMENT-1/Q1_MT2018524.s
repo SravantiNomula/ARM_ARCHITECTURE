c8cb1702e235ff1b501a4506c4e888f2a97eeda2
SravantiNomula <47375870+SravantiNomula@users.noreply.github.com>
2019-11-08 13:03:20 +0530

    Update Q1_MT2018524.s

--- ASSIGNMENT-1/Q1_MT2018524.s
+++ ASSIGNMENT-1/Q1_MT2018524.s
@@ -2,24 +2,30 @@
 ;SRAVANTI NOMULA : MT2018524
 ;FIBONACCI SERIES
 
-THUMB
+	    
+	 ;output:R2
+	 ;fib(0) to fib(N) of fibonacci series
+	 THUMB
 	 AREA     first, CODE, READONLY
-     EXPORT __main
-     ENTRY 
-__main  FUNCTION	
- 	
-	    MOV r1,#0 ; (f(i-2) = f(0) = 0)
-        MOV r2,#1 ; (f(i-1) = f(1) = 1)
-		MOV r3,#8 ; (eg:n, display first n numbers)
-loop	CMP r3,#0 ; (loop iterator check (n>0))		
-		SUBGT r3,r3,#1 ; (decrement iterator by 1)
-		BEQ stop; jump to stop once n=0
-		MOV r5,r1 ; (store r1, it stores and displays the fibonacci numbers)
-		ADD r4,r1,r2 ; (f(i) = f(i-1)+f(i-2))
-		MOV r1,r2 ; (f(i-2) = f(i-1))
-		MOV r2,r4 ; (f(i-1) = f(i))
-		BGT loop; (loop if n>0)
-stop    B stop ; stop program
+    	 EXPORT __main
+     	 ENTRY 
+__main  FUNCTION		 		
+		MOV  r0, #7 ;input r0 = N
+		MOV  r1, #0 ;fib(0)
+		MOV  r2, #1 ;fib(1)
+		SUB  r0, #1	;r0--	
+		
+loop	CMP r0, #0	;r0 == 0? to stop the iterations
+		BLE stop	;YES stop branch only when comparision result
+		
+		ADD r1, r2	;r1 = r2+r1
+		EOR r1, r2	;swap r1 and r2
+		EOR r2, r1
+		EOR r1, r2
+		SUB r0, #1	;r0--
+		B loop		;goto LOOP
+
 	   
+stop    B stop ; stop program
      ENDFUNC
      END 
