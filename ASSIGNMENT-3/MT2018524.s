3632b10cab28417acebfdd47838797d9c1bad2a3
SravantiNomula <47375870+SravantiNomula@users.noreply.github.com>
2019-11-26 12:39:35 +0530

    Update MT2018524.s

--- ASSIGNMENT-3/MT2018524.s
+++ ASSIGNMENT-3/MT2018524.s
@@ -14,62 +14,58 @@
 		ENTRY
 
 __main FUNCTION
-	
+
 	 MOV R4,#0;	a1
-	 VMOV.F32 S0,R4;			Move A1 to S0 (floating point register)
-     VCVT.F32.S32 S0,S0; 		Convert into signed 32bit number
+	 VMOV.F32 S0,R4;			Move A1 tofloating point register
+     VCVT.F32.S32 S0,S0; 		      Convert into signed 32bit number
 
 	 MOV R5,#0;	a2	
-	 VMOV.F32 S1,R5;			Move A2 to S1 (floating point register)
-     VCVT.F32.S32 S1,S1; 		Convert into signed 32bit number
-	 
-	 
-	 MOV R6,#0;	a3
-	 VMOV.F32 S2,R6;			Move A3 to S2 (floating point register)
-     VCVT.F32.S32 S2,S2; 		Convert into signed 32bit number
-	 
-	 
-	 MOV R7,#0;	Case select
-	 
+	 VMOV.F32 S1,R5;			Move A2 to floating point register
+     VCVT.F32.S32 S1,S1; 		       Convert into signed 32bit number
 
-	 
-	
-	 
+
+	 MOV R6,#1;	a3
+	 VMOV.F32 S2,R6;			Move A3 to floating point register
+     VCVT.F32.S32 S2,S2; 		       Convert into signed 32bit number
+
+
+	 MOV R7,#3;	Case selected
+
 	 ; Switch - Case
 	 CMP R7,#0;
-	 BNE L1;   If switch case is 0 do AND operation
+	 BNE Loop1;   If switch case is 0 do AND operation
 	 BL __and;					If case = 0, AND 
-	 B L8;
-	 
-L1	 CMP R7,#1;
-	 BNE L2;    If switch case is 1 do OR operation
+	 B Loop8;
+
+Loop1	 CMP R7,#1;
+	 BNE Loop2;    If switch case is 1 do OR operation
 	 BL __or;					If case = 1, OR 
-	 B L8;
-L2	 CMP R7,#2;
-	 BNE L3;	If switch case is 2 do NOT operation
+	 B Loop8;
+Loop2	 CMP R7,#2;
+	 BNE Loop3;	If switch case is 2 do NOT operation
 	 BL __not;					If case = 2, NOT
-	 B L8;
-L3	 CMP R7,#3;
-	 BNE L4;     If switch case is 3 do XOR operation
+	 B Loop8;
+Loop3	 CMP R7,#3;
+	 BNE Loop4;     If switch case is 3 do XOR operation
 	 BL __xor;					If case = 3, XOR
-	 B L8;
-L4	 CMP R7,#4;
-	 BNE L5;	 If switch case is 4 do XNOR operation
+	 B Loop8;
+Loop4	 CMP R7,#4;
+	 BNE Loop5;	 If switch case is 4 do XNOR operation
 	 BL __xnor;					If case = 4, XNOR
-	 B L8;	
-L5	 CMP R7,#5;	  
-	 BNE L6;	If switch case is 5 do NAND operation
+	 B Loop8;	
+Loop5	 CMP R7,#5;	  
+	 BNE Loop6;	If switch case is 5 do NAND operation
 	 BL __nand;					If case = 5, NAND 
-	 B L8;
-L6	 CMP R7,#6;
-	 BNE L7;	If switch case is 6 do NOR operation
+	 B Loop8;
+Loop6	 CMP R7,#6;
+	 BNE Loop7;	If switch case is 6 do NOR operation
 	 BL __nor;					If case = 6, NOR 
-	 B L8;
-L7	 
+	 B Loop8;
+Loop7	 
 	 BL printMsginvalid;	 If none of the above cases invalid operation thus print it
 	 B stop;	 
-	 
-L8
+
+Loop8
 	 BL printMsg;
 	 B stop;
 	 ENDFUNC
@@ -80,19 +76,19 @@ __sigmoid FUNCTION
 	 ;Compute sigmoid function 
 	 PUSH {LR};
 	 VLDR.F32 S8,= 1;			
-	 VADD.F32 S9,S11,S8;		 (e^-x)+1
+	 VADD.F32 S9,S11,S8;		        (e^-x)+1
 	 VDIV.F32 S9,S8,S9;			 1 / (e^-x)+1  i.e sigmoid function
 	 POP {LR};	
 	 BX lr;
 	ENDFUNC
 ;---------------------------------------------------------------------------------------
 
-__val FUNCTION
+__eval FUNCTION
 	 PUSH {LR}
 	 BL __exp;					Compute e^-x
 	 BL __sigmoid;				Sigmoid function 
-	 
-	 VLDR.F32 S14,= 0.5;		Store 0.5 in S14
+
+	 VLDR.F32 S14,= 0.5;		        Store 0.5 for threshold
 	 VCMP.F32 S9,S14;			Compare current Y and 0.5		
 	 VMRS    APSR_nzcv, FPSCR;
 	 MOVGT	R0, #1;				If Y > 0.5, output is 1
@@ -101,26 +97,26 @@ __val FUNCTION
 	 BX lr
 	 ENDFUNC	 
 
-	
+
 ;		logic AND
 
 __and FUNCTION
 	 PUSH {LR};	 
-	 VLDR.F32 S4,= -0.1;			Weight 1
-	 VLDR.F32 S5,= 0.2;			Weight 2
-	 VLDR.F32 S6,= 0.2;			Weight 3
+	 VLDR.F32 S4,= -0.1;			W1
+	 VLDR.F32 S5,= 0.2;			W2
+	 VLDR.F32 S6,= 0.2;			W3
 	 VLDR.F32 S7,= -0.2;		    Bias
-	 
+
 	 VMUL.F32 S0,S0,S4;			a1*w1
 	 VMUL.F32 S1,S1,S5;			a2*w2
 	 VMUL.F32 S2,S2,S6;			a3*w3
 	 VADD.F32 S3,S0,S1;			a1*w1 + a2*w2 
 	 VADD.F32 S3,S3,S2;			a1*w1 + a2*w2 + a3*w3 
 	 VADD.F32 S3,S3,S7;			a1*w1 + a2*w2 + a3*w3 + b
-	 
+
 	 VMOV.F32 S10,S3;			
-	 BL __val;
-	 
+	 BL __eval;
+
 	 POP {LR};	
 	 BX lr;
 	 ENDFUNC
@@ -128,10 +124,10 @@ __and FUNCTION
 ;		logic OR
 
 __or FUNCTION
-	 PUSH {LR};	 
-	 VLDR.F32 S4,= -0.1;			Weight 1
-	 VLDR.F32 S5,= 0.7;			Weight 2
-	 VLDR.F32 S6,= 0.7;			Weight 3
+	  PUSH {LR};	 
+	 VLDR.F32 S4,= -0.1;			W1
+	 VLDR.F32 S5,= 0.7;			W2
+	 VLDR.F32 S6,= 0.7;			W3
 	 VLDR.F32 S7,= -0.1;			Bias
 	 
 	 VMUL.F32 S0,S0,S4;			A1*W1
@@ -140,22 +136,23 @@ __or FUNCTION
 	 VADD.F32 S3,S0,S1;			A1*W1 + A2*W2 
 	 VADD.F32 S3,S3,S2;			A1*W1 + A2*W2 + A3*W3 
 	 VADD.F32 S3,S3,S7;			A1*W1 + A2*W2 + A3*W3 + B
-	 
+
 	 VMOV.F32 S10,S3;			S10 has the value of x
-	 BL __val;
+	 BL __eval;
 	 POP {LR};	
 	 BX lr;
 	 ENDFUNC
 
-;		logic NOT	We only consider 1st input (in R4) for NOT 
+;		logic NOT	
 
 __not FUNCTION
-	 PUSH {LR};	 
-	 VLDR.F32 S4,= 0.5;			Weight 1
-	 VLDR.F32 S5,= -0.7;			Weight 2
+	  PUSH {LR};	 
+	 VLDR.F32 S4,= 0.5;			W1
+	 VLDR.F32 S5,= -0.7;			W2
 	
 	 VLDR.F32 S7,= 0.1;			Bias
-	 
+;Only 2 weights were given in Python function
+
 	 VMUL.F32 S0,S0,S4;			A1*W1
 	 VMUL.F32 S1,S1,S5;			A2*W2
 	
@@ -164,113 +161,22 @@ __not FUNCTION
 	 VADD.F32 S3,S3,S7;			A1*W1 + A2*W2  + B
 	
 	 
-	 VMOV.F32 S10,S3;			S10 has the value of x
-	 BL __val;
+	 VMOV.F32 S10,S3;			
+	 BL __eval;
 	 POP {LR};	
 	 BX lr;
 	 ENDFUNC
+
+;same weights and bias were given for xor and xnor gate
 
 ;		logic XOR
 
 __xor FUNCTION
-	
-	PUSH {LR};
-	 ; Store the inputs 
-	 VMOV.F32 S19,S0;			S19 has A1
-	 VMOV.F32 S20,S1;			S20 has A2
-	 VMOV.F32 S21,S2;			S21 has A3
-	 
-	 BL __not;					Computes not for A
-			;					A' stored in R0
-	 VMOV.F32 S22,R0;			Move the count in R0 to S22 (floating point register)
-     VCVT.F32.S32 S22,S22; 		Convert into signed 32bit number	 
-	 
-	 VMOV.F32 S0,S20;			Move value of A2 to S0			
-	 BL __not;					Computes not for B
-			;					B' stored in R1
-	 VMOV.F32 S23,R0;			Move the count in R1 to S23 (floating point register)
-     VCVT.F32.S32 S23,S23; 		Convert into signed 32bit number
-	 
-	 VMOV.F32 S0,S21;			Move value of A3 to S0	
-	 BL __not;					Computes not for C
-			;					C' stored in R2
-	 VMOV.F32 S24,R0;			Move the count in R2 to S24 (floating point register)
-     VCVT.F32.S32 S24,S24; 		Convert into signed 32bit number
-	 
-;1)	 A1'*A2*A3'	
-	 VMOV.F32 S0,S22;	
-	 VMOV.F32 S1,S20;
-	 VMOV.F32 S2,S24;
-	 BL __and;					compute A1'*A2*A3' 
-	 MOV R4,R0;					Store value in R4
-	 
-;2)	 A1*A2'*A3'	
-	 VMOV.F32 S0,S19;	
-	 VMOV.F32 S1,S23;
-	 VMOV.F32 S2,S24;
-	 BL __and;					compute A1*A2'*A3' 
-	 MOV R5,R0;					Store value in R5
-
-;3)	 A1'*A2'*A3	
-	 VMOV.F32 S0,S22;	
-	 VMOV.F32 S1,S23;
-	 VMOV.F32 S2,S21;
-	 BL __and;					compute A1'*A2'*A3 
-	 MOV R6,R0;					Store value in R6
-	 
-;4)	 A1*A2*A3	
-	 VMOV.F32 S0,S19;	
-	 VMOV.F32 S1,S20;
-	 VMOV.F32 S2,S21;
-	 BL __and;					compute A1*A2*A3 
-	 MOV R7,R0;					Store value in R7	
-
-;    Compute OR for R4, R5, R6, R7
-
-	 VMOV.F32 S0,R7;			Move the count in R4 to S0 (floating point register)
-     VCVT.F32.S32 S0,S0; 		Convert into signed 32bit number
-	 VMOV.F32 S1,R6;			Move the count in R5 to S1 (floating point register)
-     VCVT.F32.S32 S1,S1; 		Convert into signed 32bit number
-	 VMOV.F32 S2,R5;			Move the count in R6 to S2 (floating point register)
-     VCVT.F32.S32 S2,S2; 		Convert into signed 32bit number
-	 
-	 BL __or;					Computes OR for R7+R6+R5
-	 MOV R7,R0;					Store value in R7	
-	 
-	 VMOV.F32 S0,R4;			Move the count in R4 to S0 (floating point register)
-     VCVT.F32.S32 S0,S0; 		Convert into signed 32bit number
-	 VMOV.F32 S1,R7;			Move the count in R4 to S0 (floating point register)
-     VCVT.F32.S32 S1,S1; 		Convert into signed 32bit number
-	 VLDR.F32 S2,= 0;			3rd input
-	 
-	 BL __or;					Computes AND for R7+R6+R5+R4
-
-	 POP {LR};		
-	 BX lr;
-	 ENDFUNC
-
-;		logic XNOR
-
-__xnor FUNCTION
-	 PUSH {LR};		
-
-	 BL __xor;
-	 VMOV.F32 S0,R0;			Move the count in R4 to S0 (floating point register)
-     VCVT.F32.S32 S0,S0; 		Convert into signed 32bit number
-	 BL __not;
-	 
-	 POP {LR};	
-	 BX lr;
-	 ENDFUNC
-
-;		logic NAND
-
-__nand FUNCTION
-	 PUSH {LR};	 
-	 VLDR.F32 S4,= 0.6;			Weight 1
-	 VLDR.F32 S5,= -0.8;			Weight 2
-	 VLDR.F32 S6,= -0.8;			Weight 3
-	 VLDR.F32 S7,= 0.3;			Bias
+	PUSH {LR};	 
+	 VLDR.F32 S4,= -5;			W1
+	 VLDR.F32 S5,= 20;			W2
+	 VLDR.F32 S6,= 10;			W3
+	 VLDR.F32 S7,= 1;			Bias
 	 
 	 VMUL.F32 S0,S0,S4;			A1*W1
 	 VMUL.F32 S1,S1,S5;			A2*W2
@@ -278,9 +184,56 @@ __nand FUNCTION
 	 VADD.F32 S3,S0,S1;			A1*W1 + A2*W2 
 	 VADD.F32 S3,S3,S2;			A1*W1 + A2*W2 + A3*W3 
 	 VADD.F32 S3,S3,S7;			A1*W1 + A2*W2 + A3*W3 + B
+
+	 VMOV.F32 S10,S3;			
+	 BL __eval;
+	 POP {LR};	
+	 BX lr;
+	 ENDFUNC
+
+	
+
+;		logic XNOR
+
+__xnor FUNCTION
+	 PUSH {LR};	 
+	 VLDR.F32 S4,= -5;			W1
+	 VLDR.F32 S5,= 20;			W2
+	 VLDR.F32 S6,= 10;			W3
+	 VLDR.F32 S7,= 1;			Bias
 	 
-	 VMOV.F32 S10,S3;			S10 has the value of x
-	 BL __val;
+	 VMUL.F32 S0,S0,S4;			A1*W1
+	 VMUL.F32 S1,S1,S5;			A2*W2
+	 VMUL.F32 S2,S2,S6;			A3*W3
+	 VADD.F32 S3,S0,S1;			A1*W1 + A2*W2 
+	 VADD.F32 S3,S3,S2;			A1*W1 + A2*W2 + A3*W3 
+	 VADD.F32 S3,S3,S7;			A1*W1 + A2*W2 + A3*W3 + B
+
+	 VMOV.F32 S10,S3;			
+	 BL __eval;
+	 POP {LR};	
+	 BX lr;
+	 ENDFUNC
+
+;		logic NAND
+
+__nand FUNCTION
+	  PUSH {LR};	 
+	 VLDR.F32 S4,= 0.6;			W1
+	 VLDR.F32 S5,= -0.8;			W2
+	 VLDR.F32 S6,= -0.8;			W3
+	 VLDR.F32 S7,= 0.3;			Bias
+	 
+
+	 VMUL.F32 S0,S0,S4;			A1*W1
+	 VMUL.F32 S1,S1,S5;			A2*W2
+	 VMUL.F32 S2,S2,S6;			A3*W3
+	 VADD.F32 S3,S0,S1;			A1*W1 + A2*W2 
+	 VADD.F32 S3,S3,S2;			A1*W1 + A2*W2 + A3*W3 
+	 VADD.F32 S3,S3,S7;			A1*W1 + A2*W2 + A3*W3 + B
+
+	 VMOV.F32 S10,S3;			
+	 BL __eval;
 	 POP {LR};	
 	 BX lr;
 	 ENDFUNC
@@ -289,24 +242,24 @@ __nand FUNCTION
 
 __nor FUNCTION
 	 PUSH {LR};	 
-	 VLDR.F32 S4,= 0.5;			Weight 1
-	 VLDR.F32 S5,= -0.7;			Weight 2
-	 VLDR.F32 S6,= -0.7;			Weight 3
+	 VLDR.F32 S4,= 0.5;			W1
+	 VLDR.F32 S5,= -0.7;			W2
+	 VLDR.F32 S6,= -0.7;			W3
 	 VLDR.F32 S7,= 0.1;			Bias
-	 
+
 	 VMUL.F32 S0,S0,S4;			A1*W1
 	 VMUL.F32 S1,S1,S5;			A2*W2
 	 VMUL.F32 S2,S2,S6;			A3*W3
 	 VADD.F32 S3,S0,S1;			A1*W1 + A2*W2 
 	 VADD.F32 S3,S3,S2;			A1*W1 + A2*W2 + A3*W3 
 	 VADD.F32 S3,S3,S7;			A1*W1 + A2*W2 + A3*W3 + B
-	 
-	 VMOV.F32 S10,S3;			S10 has the value of x
-	 BL __val;
+
+	 VMOV.F32 S10,S3;			
+	 BL __eval;
 	 POP {LR};	
 	 BX lr;
 	 ENDFUNC
-	  		 
+
 ;--------------------------------------------------------------------------------------------------
 
 stop B stop
