ee4f5b93674aa1e55c9125827d99e4ab4f18d173
SravantiNomula <47375870+SravantiNomula@users.noreply.github.com>
2019-12-05 00:29:16 +0530

    Update printMsg.c

--- ASSIGNMENT-3/printMsg.c
+++ ASSIGNMENT-3/printMsg.c
@@ -1,5 +1,6 @@
 #include "stm32f4xx.h"
 #include <string.h>
+#include<stdio.h>
 
 void printMsginvalid()
 {
@@ -24,3 +25,71 @@ void printMsg(const int a)
    }
 }
 
+void printMsg4p(const int a, const int b, const int c, const int d)
+{
+	 char Msg[100];
+	 char *ptr;
+	 // Printing the message
+	sprintf(Msg,"Truth Table \n X1:  ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 //Printing the first parameter
+	 sprintf(Msg, "%x", a);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 // Printing the message
+	 sprintf(Msg,"\t X2: ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", b);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 // Printing the message
+	 sprintf(Msg,"\t X3: ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", c);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 // Printing the message
+	 sprintf(Msg,"\t OUTPUT: ");
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+   }
+	 sprintf(Msg, "%x", d);
+	 ptr = Msg ;
+   while(*ptr != '\0')
+	 {
+      ITM_SendChar(*ptr);
+      ++ptr;
+	 }
+
+	
+}
