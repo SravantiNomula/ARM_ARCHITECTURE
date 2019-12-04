bee3e80d95ea72da9c3b9eeee3bb389ccebc0134
SravantiNomula <47375870+SravantiNomula@users.noreply.github.com>
2019-12-05 00:28:16 +0530

    Update MT2018524.s

--- ASSIGNMENT-3/MT2018524.s
+++ ASSIGNMENT-3/MT2018524.s
@@ -1,35 +1,35 @@
  ;a1,a2,a3 are the 3 inputs, w1,w2,w3 are the 3 outputs
- ; R4---a1
- ;R5----a2
- ;R6----a3
+ ; R0---a1
+ ;R1----a2
+ ;R2----a3
  ;R7-- case select if (0 and) (1 or) (2 not) (3 xor) (4 xnor) (5 nand)  (6 nor)
 
     THUMB
 	PRESERVE8	
 	AREA  first, CODE,READONLY
 	EXPORT __main
-	IMPORT printMsg
+	IMPORT printMsg4p
 	IMPORT printMsginvalid
-	IMPORT __exp
-		ENTRY
+	
+	ENTRY
 
 __main FUNCTION
 
-	 MOV R4,#0;	a1
-	 VMOV.F32 S0,R4;			Move A1 tofloating point register
+	 MOV R0,#1;	a1
+	 VMOV.F32 S0,R0;			Move A1 tofloating point register
      VCVT.F32.S32 S0,S0; 		      Convert into signed 32bit number
 
-	 MOV R5,#0;	a2	
-	 VMOV.F32 S1,R5;			Move A2 to floating point register
+	 MOV R1,#1;	a2	
+	 VMOV.F32 S1,R1;			Move A2 to floating point register
      VCVT.F32.S32 S1,S1; 		       Convert into signed 32bit number
 
 
-	 MOV R6,#1;	a3
-	 VMOV.F32 S2,R6;			Move A3 to floating point register
+	 MOV R2,#1;	a3
+	 VMOV.F32 S2,R2;			Move A3 to floating point register
      VCVT.F32.S32 S2,S2; 		       Convert into signed 32bit number
 
 
-	 MOV R7,#3;	Case selected
+	 MOV R7,#0;	Case selected
 
 	 ; Switch - Case
 	 CMP R7,#0;
@@ -66,7 +66,7 @@ Loop7
 	 B stop;	 
 
 Loop8
-	 BL printMsg;
+	 BL printMsg4p;
 	 B stop;
 	 ENDFUNC
 
@@ -91,8 +91,8 @@ __eval FUNCTION
 	 VLDR.F32 S14,= 0.5;		        Store 0.5 for threshold
 	 VCMP.F32 S9,S14;			Compare current Y and 0.5		
 	 VMRS    APSR_nzcv, FPSCR;
-	 MOVGT	R0, #1;				If Y > 0.5, output is 1
-	 MOVLT	R0, #0;				If Y < 0.5, output is 0
+	 MOVGT	R3, #1;				If Y > 0.5, output is 1
+	 MOVLT	R3, #0;				If Y < 0.5, output is 0
 	 POP {LR}
 	 BX lr
 	 ENDFUNC	 
@@ -259,6 +259,34 @@ __nor FUNCTION
 	 POP {LR};	
 	 BX lr;
 	 ENDFUNC
+
+__exp FUNCTION
+	 ; S10 has value of x
+	 VNEG.F32 S10,S10;			S10 has -x
+	 
+	 MOV R8,#200;				No of terms in the series
+     MOV R9,#1;	    			Count
+	 
+     VLDR.F32 S11,=1;			Store value of e^x
+     VLDR.F32 S12,=1;			Temp variable to hold the previous term     
+	 
+LOOP9 
+	 CMP R9,R8;					Compare count and no of term
+     BLE LOOP10;				If count is < no og terms enter LOOP1	
+     BX LR;						else STOP
+	 
+LOOP10  
+	 VMUL.F32 S12,S12,S10; 		Temp_var = temp_var * x
+     VMOV.F32 S13,R9;			Move the count in R9 to S13 (floating point register)
+     VCVT.F32.S32 S13,S13; 		Convert into signed 32bit number
+     VDIV.F32 S12,S12,S13;		Divide temp_var by count (Now the term is finished)
+     VADD.F32 S11,S11,S12;		Add temp_var to the sum
+     ADD R9,R9,#1;				Increment the count
+     B LOOP9;
+	 
+	 ENDFUNC
+	
+
 
 ;--------------------------------------------------------------------------------------------------
 
